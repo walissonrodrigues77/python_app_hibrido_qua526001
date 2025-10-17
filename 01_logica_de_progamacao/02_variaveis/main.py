1cb29b0a79a3fb1939c455617dba2174ca92eeba
walissonrodrigues <walissondeejhay@gmail.com>
2025-10-17 16:40:09 -0300

    meu primeiro commit. aula do dia 17/10/2025.

---
+++ 01_logica_de_progamacao/02_variaveis/main.py
@@ -0,0 +1,13 @@
+# declaração de variaveis
+nome_completo = "walisson Rodrigues" 
+idade = 35 # int numeros inteiros
+altura = 1.71 # float
+programador = True # bool (boolean)
+faculdade = False
+
+# "saida de dados
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
+print(faculdade)
---
+++ 01_logica_de_progamacao/03_tipos_de_dados/main.py
@@ -0,0 +1,11 @@
+nome = "walisson Rodrigues"
+idade = 35
+altura = 1.71
+programador = True
+print (nome,idade,altura,programador)
+
+# exibe os tipos de dados
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
